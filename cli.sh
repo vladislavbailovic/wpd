d8fbe20dc9becddf302a0ef331da57d6d8202284
vladislav <ve@incsub.com>
2017-10-17 07:05:03 +0200

    Add generic fallback WP CLI action

--- cli.sh
+++ cli.sh
@@ -5,7 +5,22 @@ function wpd_cli_usage {
 	exit 1
 }
 
+if [ $# -lt 2 ]; then
+	wpd_cli_usage
+fi
+
 TARGET=${1:-}
 if [ "" == "$TARGET" ]; then
 	wpd_cli_usage
+fi
+shift
+
+WPCLI_ARGS="$@"
+CMD=${2:-}
+
+if [ "$CMD" == "" ]; then
+	echo "tbd"
+else
+	# Generic fallthrough - pass everything to WP CLI
+	docker exec "$TARGET"wp wp "$WPCLI_ARGS"
 fi
